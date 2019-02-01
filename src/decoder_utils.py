ca6001cb986b66d1eb86edbd323d834b0a4c8a43
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-02-01 12:05:49 +0100

    deeper cleanup

--- src/decoder_utils.py
+++ src/decoder_utils.py
@@ -4,6 +4,7 @@
 
 """
 Module name: decoder_utils, based on dso200
+
 dso200 is Copyright (c) 2014 Good Will Instrument Co., Ltd All Rights Reserved.
 Copyright (c) 2019 Stefano Sinigardi, Coesia, All Rights Reserved.
 
@@ -16,23 +17,14 @@ See the GNU Lesser General Public License for more details.
 You can receive a copy of the GNU Lesser General Public License from http://www.gnu.org/
 """
 
-from PIL import Image
-from struct import unpack
+import struct
 import numpy as np
 import array
 import struct
-import os, sys, time
-
-def generate_lut():
-  global lu_table
-  num=65536
-  lu_table=[]
-  for i in range(num):
-    pixel888=[0]*3
-    pixel888[0]=(i>>8)&0xf8
-    pixel888[1]=(i>>3)&0xfc
-    pixel888[2]=(i<<3)&0xf8
-    lu_table.append(pixel888)
+import matplotlib.pyplot as plt
+import os
+import sys
+import time
 
 class Dso200:
   def __init__(self):
@@ -44,126 +36,123 @@ class Dso200:
     self.hpos=[[], []]
     self.ch_list=[]
     self.info=[[], []]
-    generate_lut()
-
-  def convertWaveform(self, ch, factor):
-    dv=self.vdiv[ch]/25
-    if(factor==1):
-      num=self.points_num
-      fWave=[0]*num
-      for x in range(num):       #Convert 16 bits signed to floating point number.
-        fWave[x]=float(self.iWave[ch][x])*dv
-    else: #Reduced to helf points.
-      num=self.points_num/factor
-      fWave=[0]*num
-      for x in range(num):       #Convert 16 bits signed to floating point number.
-        i=factor*x
-        fWave[x]=float(self.iWave[ch][i])*dv
-    return fWave
 
   def readRawDataFile(self,  fileName):
-    #Check file format(csv or lsf)
     self.info=[[], []]
-    if(fileName.lower().endswith('.csv')):
-      self.dataType='csv'
-    elif(fileName.lower().endswith('.lsf')):
-      self.dataType='lsf'
-    else:
-      return -1
     f = open(fileName, 'rb')
-    info=[]
-    #Read file header.
-    if(self.dataType=='csv'):
-      for x in range(26):
-        info.append(f.readline().split(',\n')[0])
-      if(info[0].split(',')[1]!='0.20'): #Check format version
-        f.close()
-        return -1
-      count=info[5].count('CH')  #Check channel number in file.
-      wave=f.read().splitlines() #Read raw data from file.
-      self.points_num=len(wave)
-    else:
-      info=f.readline().split(';') #The last item will be '\n'.
-      if(info[0].split('Format,')[1]!='0.20'): #Check format version
-        f.close()
-        return -1
-      if(f.read(1)!='#'):
-        print('Format error!')
-        sys.exit(0)
-      digit=int(f.read(1))
-      num=int(f.read(digit))
-      count=1
-      wave=f.read() #Read raw data from file.
-      self.points_num=len(wave)/2   #Calculate sample points length.
+    size = os.path.getsize(fileName)
+    info = []
+
+    # Read file header.
+    infob=f.readline().split(b';')             #The last item will be b'\n' (the b is because the file is opened in binary mode).
+    info = [x.decode('utf-8') for x in infob]  #decode the list from binary item to strings
+
+    if(f.read(1)!=b'#'):
+      print('Format error!')
+      sys.exit(0)
+    digit=int(f.read(1))
+    num=int(f.read(digit))
+    wave=f.read() #Read raw data from file.
+    self.points_num=len(wave)/2   #Calculate sample points length.
     f.close()
 
-    if(count==1): #1 channel
-      ch=0
-      self.iWave[ch]=[0]*self.points_num
-      sCh=[s for s in info if "Source" in s]
-      self.ch_list.append(sCh[0].split(',')[1])
-      sVunit = [s for s in info if "Vertical Units" in s]
-      self.vunit[ch] =sVunit[0].split(',')[1]    #Get vertical units.
-      sDv = [s for s in info if "Vertical Scale" in s]
-      self.vdiv[ch] = float(sDv[0].split(',')[1])  #Get vertical scale. => Voltage for ADC's single step.
-      sVpos=[s for s in info if "Vertical Position" in s]
-      self.vpos[ch] =float(sVpos[0].split(',')[1]) #Get vertical position.
-      sHpos = [s for s in info if "Horizontal Position" in s]
-      self.hpos[ch] =float(sHpos[0].split(',')[1]) #Get horizontal position.
-      sDt = [s for s in info if "Sampling Period" in s]
-      self.dt[ch]=float(sDt[0].split(',')[1])    #Get sample period.
-      dv1=self.vdiv[ch]/25
-      vpos=int(self.vpos[ch]/dv1)+128
-      num=self.points_num
-      if(self.dataType=='csv'):
-        for x in range(26):
-          self.info[ch].append(info[x])
-        for x in range(num):
-          value=int(wave[x].split(',')[0])
-          self.iWave[ch][x]=value
-      else: #lsf file
-        for x in range(24):
-          self.info[ch].append(info[x])
-        self.info[ch].append('Mode,Fast') #Convert info[] to csv compatible format.
-        self.info[ch].append('Waveform Data')
-        self.iWave[ch] = np.array(unpack('<%sh' % (len(wave)/2), wave))
-        for x in range(num):      #Convert 16 bits signed number to floating point number.
-          self.iWave[ch][x]-=vpos
-      del wave
-      return 1
-    elif(count==2): #2 channel, csv file only.
-      #write waveform's info to self.info[]
-      for ch in range(count):
-        self.info[ch].append(info[0])
-      for x in range(1, 26):
-        str=info[x].split(',')
-        for ch in range(count):
-          self.info[ch].append('%s,%s'%(str[2*ch],  str[2*ch+1]))
-      for ch in range(count):
-        self.iWave[ch]=[0]*self.points_num
-        sCh=[s for s in info if "Source" in s]
-        self.ch_list.append(sCh[0].split(',')[2*ch+1])
-        sVunit = [s for s in info if "Vertical Units" in s]
-        self.vunit[ch] =sVunit[0].split(',')[2*ch+1]    #Get vertical units.
-        sDv = [s for s in info if "Vertical Scale" in s]
-        self.vdiv[ch] = float(sDv[0].split(',')[2*ch+1])  #Get vertical scale. => Voltage for ADC's single step.
-        sVpos=[s for s in info if "Vertical Position" in s]
-        self.vpos[ch] =float(sVpos[0].split(',')[2*ch+1]) #Get vertical position.
-        sHpos = [s for s in info if "Horizontal Position" in s]
-        self.hpos[ch] =float(sHpos[0].split(',')[2*ch+1]) #Get horizontal position.
-        sDt = [s for s in info if "Sampling Period" in s]
-        self.dt[ch]=float(sDt[0].split(',')[2*ch+1])    #Get sample period.
+    ch=0
 
-      num=self.points_num
-      for ch in range(count):
-        self.iWave[ch]=[0]*num
-      for i in range(num):
-        str=wave[i].split(',')
-        for ch in range(count):
-          index=2*ch
-          self.iWave[ch][i]=int(str[index])
-    else:
-      return -1
-
+    sCh=[s for s in info if "Source" in s]
+    self.ch_list.append(sCh[0].split(',')[1])
+    sVunit = [s for s in info if "Vertical Units" in s]
+    self.vunit[ch] =sVunit[0].split(',')[1]    #Get vertical units.
+    sDv = [s for s in info if "Vertical Scale" in s]
+    self.vdiv[ch] = float(sDv[0].split(',')[1])  #Get vertical scale. => Voltage for ADC's single step.
+    sVpos=[s for s in info if "Vertical Position" in s]
+    self.vpos[ch] =float(sVpos[0].split(',')[1]) #Get vertical position.
+    sHpos = [s for s in info if "Horizontal Position" in s]
+    self.hpos[ch] =float(sHpos[0].split(',')[1]) #Get horizontal position.
+    sDt = [s for s in info if "Sampling Period" in s]
+    self.dt[ch]=float(sDt[0].split(',')[1])    #Get sample period.
+    dv1=self.vdiv[ch]/25
+    vpos=int(self.vpos[ch]/dv1)+128
+    num=int(self.points_num)
+    for x in range(24):
+      self.info[ch].append(info[x])
+    self.info[ch].append('Mode,Fast') #Convert info[] to csv compatible format.
+    self.info[ch].append('Waveform Data')
+    self.iWave[ch] = np.array(struct.unpack('<%sh' % (len(wave) // 2), wave))
+    for x in range(num):      #Convert 16 bits signed number to floating point number.
+      self.iWave[ch][x]-=vpos
     del wave
-    return count
+    return 1
+
+
+class oscilloscope_data:
+  def __init__(self, parent=None):
+    self.figure = plt.figure()
+    self.figure.set_facecolor('white')
+    self.typeFlag = True  # Initial state -> Get raw data
+    self.dso = Dso200()
+
+  def save_csv(self):
+    if(self.typeFlag == True):  # Save raw data to csv file.
+      file_name = "test.csv"
+      num = len(self.dso.ch_list)
+      for ch in range(num):
+        if(self.dso.info[ch] == []):
+          print('Failed to save data, raw data information is required!')
+          return
+      f = open(file_name, 'w')
+      item = len(self.dso.info[0])
+      #Write file header.
+      f.write('%s,\r\n' % self.dso.info[0][0])
+      for x in range(1,  25):
+        str = ''
+        for ch in range(num):
+          str += ('%s,' % self.dso.info[ch][x])
+        str += '\r\n'
+        f.write(str)
+      str = ''
+      if(num == 1):
+        str += ('%s,' % self.dso.info[0][25])
+      else:
+        for ch in range(num):
+          str += ('%s,,' % self.dso.info[ch][25])
+      str += '\r\n'
+      f.write(str)
+      #Write raw data.
+      item = len(self.dso.iWave[0])
+      tenth = int(item/10)
+      n_tenth = tenth-1
+      percent = 10
+      for x in range(item):
+        str = ''
+        if(num == 1):
+          str += ('%s,' % self.dso.iWave[0][x])
+        else:
+          for ch in range(num):
+            str += ('%s,,' % self.dso.iWave[ch][x])
+        str += '\r\n'
+        f.write(str)
+        if(x == n_tenth):
+          n_tenth += tenth
+          print('%3d %% Saved\r' % percent),
+          percent += 10
+      f.close()
+
+  def load_lsf(self):
+    self.dso.ch_list = []
+    sFileName = "test.lsf"
+    if os.path.exists(sFileName):
+      print('Reading file...')
+      count = self.dso.readRawDataFile(sFileName)
+      #Print diagnostic data
+      if(count > 0):
+        total_chnum = len(self.dso.ch_list)
+        if(total_chnum == 0):
+          return
+        print(total_chnum)
+    else:
+      print('File not found!')
+
+  def run(self):
+    main = oscilloscope_data()
+    main.load_lsf()
+    main.save_csv()
--- src/lsf_decoder.py
+++ src/lsf_decoder.py
@@ -4,6 +4,7 @@
 
 """
 Program name: lsf_decoder, based on OpenWave-200
+
 OpenWave-200 is Copyright (c) 2014 Good Will Instrument Co., Ltd All Rights Reserved.
 Copyright (c) 2019 Stefano Sinigardi, Coesia, All Rights Reserved.
 
@@ -16,83 +17,9 @@ See the GNU Lesser General Public License for more details.
 You can receive a copy of the GNU Lesser General Public License from http://www.gnu.org/
 """
 
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-from mpl_toolkits.axes_grid1 import host_subplot
-import mpl_toolkits.axisartist as AA
-import numpy as np
-import os, sys, time
-import decoder_utils
+import decoder_utils as du
 
-class oscilloscope_data():
-  def __init__(self, parent=None):
-    self.figure = plt.figure()
-    self.figure.set_facecolor('white')
-    self.typeFlag = True  # Initial state -> Get raw data
-
-  def save_csv(self):
-    if(self.typeFlag==True): #Save raw data to csv file.
-      file_name="test.csv"
-      num=len(dso.ch_list)
-      for ch in range(num):
-        if(dso.info[ch]==[]):
-          print('Failed to save data, raw data information is required!')
-          return
-      f = open(file_name, 'wb')
-      item=len(dso.info[0])
-      #Write file header.
-      f.write('%s,\r\n' % dso.info[0][0])
-      for x in range(1,  25):
-        str=''
-        for ch in range(num):
-          str+=('%s,' % dso.info[ch][x])
-        str+='\r\n'
-        f.write(str)
-      str=''
-      if(num==1):
-        str+=('%s,' % dso.info[0][25])
-      else:
-        for ch in range(num):
-          str+=('%s,,' % dso.info[ch][25])
-      str+='\r\n'
-      f.write(str)
-      #Write raw data.
-      item=len(dso.iWave[0])
-      tenth=int(item/10)
-      n_tenth=tenth-1
-      percent=10
-      for x in range(item):
-        str=''
-        if(num==1):
-          str+=('%s,' % dso.iWave[0][x])
-        else:
-          for ch in range(num):
-            str+=('%s,,' % dso.iWave[ch][x])
-        str+='\r\n'
-        f.write(str)
-        if(x==n_tenth):
-          n_tenth+=tenth
-          print('%3d %% Saved\r'%percent),
-          percent+=10
-      f.close()
-
-  def load_lsf(self):
-    dso.ch_list=[]
-    sFileName="test.lsf"
-    if os.path.exists(sFileName):
-      print('Reading file...')
-      count=dso.readRawDataFile(sFileName)
-      #Print diagnostic data
-      if(count>0):
-        total_chnum=len(dso.ch_list)
-        if(total_chnum==0):
-          return
-        print(total_chnum)
-    else:
-      print('File not found!')
 
 if __name__ == '__main__':
-  dso=decoder_utils.Dso200()
-  main = oscilloscope_data()
-  main.load_lsf()
-  main.save_csv()
+  a = du.oscilloscope_data()
+  a.run()
